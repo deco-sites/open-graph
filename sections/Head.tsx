e0d314376b11cefe618522e6aafbfc93282af646
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2023-03-16 11:19:36 -0300

    feat: add liveimage instead of image url

--- sections/Head.tsx
+++ sections/Head.tsx
@@ -1,11 +1,12 @@
-import { asset, Head } from "$fresh/runtime.ts";
+import { Head } from "$fresh/runtime.ts";
+import { Image as LiveImage } from "$live/std/ui/types/Image.ts";
 import Header from "../components/Header.tsx";
 import ViewOpenGraph from "../components/ViewOpenGraph.tsx";
 export interface Props {
   title: string;
   description: string;
   url: string;
-  imageUrl: string;
+  image: LiveImage;
   type: string;
   themeColor: string;
 }
@@ -14,23 +15,24 @@ export default function HeadComponent({
   title = "deco.cx — starter site",
   description = "Deliver complete commerce experiences — start here!",
   url = "https://start.deco.site",
-  imageUrl = "https://deco.cx/images/deco-logo-light.png",
+  image = "https://deco.cx/images/deco-logo-light.png",
   type = "website",
   themeColor = "#003232",
 }: Props) {
   return (
     <>
       <Head>
-        <title>{title}</title>
-        <meta name="theme-color" content={themeColor} />
-        <meta name="description" content={description} />
-
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:type" content={type} />
         <meta property="og:url" content={url} />
-        <meta property="og:image" content={imageUrl} />       
+        <meta property="og:image" content={image} /> 
+
+        <title>{title}</title>
+        <meta name="theme-color" content={themeColor} />
+        <meta name="description" content={description} />      
       </Head>
+      
       <Header />
       <ViewOpenGraph />
     </>
