1ef86135fcfc38f6b646cdc982223d71c5363a2d
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-03-17 16:27:15 -0300

    feat: creating LinkedIn Open Graph component

---
+++ components/Head/Preview/LinkedIn.tsx
@@ -0,0 +1,33 @@
+import { Image as LiveImage } from "$live/std/ui/types/Image.ts";
+import Image from "$live/std/ui/components/Image.tsx";
+
+interface LinkedInProps {
+  image: LiveImage;
+  title: string;
+}
+
+export default function LinkedIn(props: LinkedInProps) {
+  const { image, title } = props;
+
+  return (
+    <div>
+      <Image
+        src={image}
+        alt="Grogu"
+        class="w-full h-[210px] sm:h-[273px] w-[400px] sm:w-[552px] object-cover"
+        decoding="async"
+        loading="lazy"
+        width={200}
+        height={200}
+      />
+      <div class="px-4 py-3 bg-linkedin-bg ">
+        <p class="text-sm font-thin font-inter text-secondary leading-[19px]">
+          {title}
+        </p>
+        <p class="text-xs font-normal text-common leading-[19px]">
+          website.com
+        </p>
+      </div>
+    </div>
+  );
+}
