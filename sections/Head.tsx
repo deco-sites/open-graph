7d66cc625c584ec74bd1fa5576e9f4f3139786bc
loliveirawebdev <loliveirawebdev@gmail.com>
2023-03-17 10:59:36 -0300

    chore: add lint changes

--- sections/Head.tsx
+++ sections/Head.tsx
@@ -1,3 +1,3 @@
 export { default } from "$start/components/Head/Head.tsx";
 export type { Image as LiveImage } from "$live/std/ui/types/Image.ts";
-export type { Props, OGType } from "$start/components/Head/inteface.tsx";
+export type { OGType, Props } from "$start/components/Head/inteface.tsx";
