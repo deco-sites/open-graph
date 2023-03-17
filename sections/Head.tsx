457fe403a5c35f68702355f2562f83c8a3bd4caa
loliveirawebdev <loliveirawebdev@gmail.com>
2023-03-17 10:08:51 -0300

    fix: change meta tags order

--- sections/Head.tsx
+++ sections/Head.tsx
@@ -29,17 +29,17 @@ export default function HeadComponent({
   return (
     <>
       <Head>
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:type" content={type} />
-        <meta property="og:url" content={url} />
-        <meta property="og:image" content={image} />
-
         <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
         <meta property="twitter:url" content={url} />
         <meta property="twitter:image" content={image} />
         <meta property="twitter:card" content={getTwitterCardValue()} />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content={type} />
+        <meta property="og:url" content={url} />
+        <meta property="og:image" content={image} />
 
         <title>{title}</title>
         <meta name="theme-color" content={themeColor} />
