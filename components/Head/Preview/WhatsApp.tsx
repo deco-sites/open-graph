09d539c84594a314511d81c8f2091b39ad7dda48
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-03-21 14:09:27 -0300

    feat: completing Whatsapp component with smaller version

--- components/Head/Preview/WhatsApp.tsx
+++ components/Head/Preview/WhatsApp.tsx
@@ -1,12 +1,13 @@
-import Title from "./fragments/Title.tsx";
 import Image from "$live/std/ui/components/Image.tsx";
+import { PreviewItens } from "../inteface.tsx";
 
-export default function WhatsApp() {
+function WhatsAppBiggerImages(props: PreviewItens) {
+  const { image, title, description } = props;
   return (
     <div class="w-[330px] h-[280px] rounded-lg overflow-hidden box-border ">
       <Image
-        src="https://uploads.metropoles.com/wp-content/uploads/2022/02/25141718/Grogu-Baby-yoda.jpg"
-        alt="Grogu"
+        src={image}
+        alt={title}
         class="w-full h-[174px] object-cover"
         decoding="async"
         loading="lazy"
@@ -23,3 +24,42 @@ export default function WhatsApp() {
     </div>
   );
 }
+
+function WhatsAppSmallerImages(props: PreviewItens) {
+  const { image, title, description, width } = props;
+  return (
+    <div class="rounded-lg overflow-hidden box-border flex">
+      <Image
+        src={image}
+        decoding="async"
+        class="object-fill"
+        loading="lazy"
+        width={106}
+        height={106}
+      />
+      <div class="text-[15px] flex flex-col px-[12px] justify-center bg-secondary leading-5">
+        <h3 class="text-secondary">Website</h3>
+        <p class="text-common">
+          A page's meta description tag is meant to give the user an idea of the
+          content that exists within the page.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Whatsapp(props: PreviewItens) {
+  const { image, title, description, width } = props;
+
+  if (width === 0) {
+    return <div></div>;
+  }
+
+  if (width < 300 && width !== 0) {
+    return <WhatsAppSmallerImages {...props} />;
+  }
+
+  return <WhatsAppBiggerImages {...props} />;
+}
+
+export default Whatsapp;
