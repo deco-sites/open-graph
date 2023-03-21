c3f7f784a0ae6c7ad2f6ab6ffa082aa86c929ba2
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-03-21 16:12:22 -0300

    feat: listing instructions about the Open Graphs

--- components/Head/Preview/PreviewItem.tsx
+++ components/Head/Preview/PreviewItem.tsx
@@ -4,12 +4,23 @@ import Title from "./fragments/Title.tsx";
 interface PreviewItemProps {
   title: string;
   children: ComponentChildren;
+  instructions: string[];
 }
 
-export default function PreviewItem({ title, children }: PreviewItemProps) {
+export default function PreviewItem(
+  { title, children, instructions }: PreviewItemProps,
+) {
   return (
-    <div class="w-[400px] sm:w-[522px]">
+    <div class="w-[400px] flex flex-col gap-[16px] sm:w-[522px]">
       <Title>{title}</Title>
+      <div class="text-[13px] leading-[20px]">
+        <p>Instructions:</p>
+        <ul>
+          {instructions.map((instruction) => (
+            <li class="list-disc list-inside">{instruction}</li>
+          ))}
+        </ul>
+      </div>
       {children}
     </div>
   );
