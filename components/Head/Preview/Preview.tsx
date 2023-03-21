f6b5d9988c48909d8a1247238d5456672c3e4e00
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-03-21 14:10:07 -0300

    feat: structuring preview

--- components/Head/Preview/Preview.tsx
+++ components/Head/Preview/Preview.tsx
@@ -1,12 +1,14 @@
 import { Dimensions, PreviewProps, Props } from "../inteface.tsx";
 import WhatsApp from "./WhatsApp.tsx";
-import { SlackArticle, SlackWebsite } from "./Slack.tsx";
 import PreviewItem from "./PreviewItem.tsx";
 import LinkedIn from "./LinkedIn.tsx";
 import { useSignal } from "@preact/signals";
 import { useRef } from "preact/hooks";
 import Facebook from "./Facebook.tsx";
 import Telegram from "./Telegram.tsx";
+import Google from "./Google.tsx";
+import Twitter from "./Twitter.tsx";
+import Slack from "./Slack.tsx";
 
 export default function PreviewHandler(propsOriginais: Props) {
   const { title, description, url, image, type, themeColor } = propsOriginais;
@@ -43,17 +45,37 @@ function Preview({ props, dimensions }: PreviewProps) {
   const { width, height } = dimensions;
 
   return (
-    <section>
-      <header class="px-10 py-8 text-primary">
+    <section class="flex flex-col items-center">
+      <header class="px-10 w-full max-w-[1156px] py-8 text-primary">
         <h1 class="font-semibold text-xl pb-1">Preview</h1>
         <p class="text-[15px]">
           How your website is displayed on search engines and social media
         </p>
       </header>
-      <div class="pl-10">
-        <PreviewItem title="Slack">
-          <Telegram {...{ ...props, ...dimensions }} />
-        </PreviewItem>
+      <div class="flex flex-col max-w-[1156px] items-center">
+        <div class="flex flex-col items-center gap-[32px] mb-[20px] lg:(grid grid-cols-2 items-start justify-center)">
+          <PreviewItem title="Goggle">
+            <Google {...{ ...props, ...dimensions }} />
+          </PreviewItem>
+          <PreviewItem title="Linkedin">
+            <LinkedIn {...{ ...props, ...dimensions }} />
+          </PreviewItem>
+          <PreviewItem title="Whatsapp">
+            <WhatsApp {...{ ...props, ...dimensions }} />
+          </PreviewItem>
+          <PreviewItem title="Telegram">
+            <Telegram {...{ ...props, ...dimensions }} />
+          </PreviewItem>
+          <PreviewItem title="Facebook">
+            <Facebook {...{ ...props, ...dimensions }} />
+          </PreviewItem>
+          <PreviewItem title="Twitter">
+            <Twitter {...{ ...props, ...dimensions }} />
+          </PreviewItem>
+          <PreviewItem title="Slack">
+            <Slack {...{ ...props, ...dimensions }} />
+          </PreviewItem>
+        </div>
       </div>
     </section>
   );
